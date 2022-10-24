e0aa5bb80f21dc63c6f98ab5014a3d5a0e0d6431
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-24 23:25:00 +0600

    route setup to get all the courses

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("Code Crate server is running!");
 });
 
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 app.listen(port, () => {
   console.log(`Code Crate is listening on port ${port}`);
 });

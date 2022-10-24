0c5177fd439434a43a88105f4b1372af3adb5dc4
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-10-24 22:46:04 +0600

    setup cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(cors());
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
